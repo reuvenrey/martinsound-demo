b3a0785ab747c32d50f69219ca1c6025d4665fa0
Reuven Reyman <94356206+reuvenrey@users.noreply.github.com>
2026-08-12 13:24:43 -0700

    Create setup.js

---
+++ setup.js
@@ -0,0 +1,113 @@
+// Navigation Contents
+headerContents = [];
+
+headerContents[0] = {
+    display:"Film Spaces", link:"filmspaces.html",
+    style:'regular'
+};
+
+headerContents[1] = {
+    display:"Rentals", link:"https://www.martinsound.com/products/",
+    style:'regular'
+}
+
+headerContents[2] = {
+    display:"About Us", link:"https://www.martinsound.com/history/",
+    style:'regular'
+}
+
+headerContents[3] = {
+    display:"FAQ", link:"faq.html",
+    style:'regular'
+}
+
+headerContents[4] = {
+    display:"Contact", link:"contact.html",
+    style:'regular'
+}
+
+filmSpaces = []; var iter=-1;
+
+    iter+=1; filmSpaces[iter] = {
+        name: "New York Subway", link: "https://www.martinsound.com/subway/", bookingLink: "",
+        dispImg: "https://www.martinsound.com/wp-content/uploads/2020/03/IMG_5553.png", 
+        shortDesc: "A full-scale New York City subway car and station.",
+        tags: "",
+        longDesc: ""
+    }
+
+    iter+=1; filmSpaces[iter] = {
+        name: "Abandonded Apartment", link: "https://www.martinsound.com/apartment/", bookingLink: "",
+        dispImg: "https://www.martinsound.com/wp-content/uploads/2020/03/DSC00042-1024x576.jpg", 
+        shortDesc: "1970s vintage apartment with grungy wallpaper and stained carpets",
+        tags: "",
+        longDesc: ""
+    }
+
+    iter+=1; filmSpaces[iter] = {
+        name: "Brick Wall Sound Stage", link: "https://www.martinsound.com/brick-wall/", bookingLink: "",
+        dispImg: "https://www.martinsound.com/wp-content/uploads/2020/03/DSC00114-1-1024x683.jpg", 
+        shortDesc: "Flexible film space with overhead mounts and a fake brick wall",
+        tags: "",
+        longDesc: ""
+    }
+
+    iter+=1; filmSpaces[iter] = {
+        name: "Grungy Stockroom", link: "https://www.martinsound.com/storage-2/", bookingLink: "",
+        dispImg: "https://www.martinsound.com/wp-content/uploads/2020/03/DSC09237-1024x683.jpg", 
+        shortDesc: "Racks and racks of old electronics equipment and florecent lighting",
+        tags: "",
+        longDesc: ""
+    }
+
+    iter+=1; filmSpaces[iter] = {
+        name: "Retro Electronics Lab", link: "https://www.martinsound.com/lab-2/", bookingLink: "",
+        dispImg: "https://www.martinsound.com/wp-content/uploads/2020/03/DSC09659-1-1024x683.jpg", 
+        shortDesc: "Racks of electronics equipment, microscopes, spare parts, soldering irons, and computers",
+        tags: "",
+        longDesc: ""
+    }
+
+    iter+=1; filmSpaces[iter] = {
+        name: "90's Retro Office Space", link: "https://www.martinsound.com/office/", bookingLink: "",
+        dispImg: "https://www.martinsound.com/wp-content/uploads/2022/04/DSC00267-1024x683.jpg", 
+        shortDesc: "Perfect for music videos, interviews, and short/feature film production.",
+        tags: "",
+        longDesc: ""
+    }
+
+    iter+=1; filmSpaces[iter] = {
+        name: "Massive Recording Studio & Sound-stage", link: "https://www.martinsound.com/studio-1-film/", bookingLink: "",
+        dispImg: "https://www.martinsound.com/wp-content/uploads/2020/03/ST1_CR_Console_Speakers_600_output_output-1024x681.jpg", 
+        shortDesc: "A 2000 sq. ft. vintage recording Studio with whisper Quiet A/C",
+        tags: "",
+        longDesc: ""
+    }
+
+    iter+=1; filmSpaces[iter] = {
+        name: "Cozy Vintage Recording Studio", link: "https://www.martinsound.com/cozy-vintage-high-end-recording-studio/", bookingLink: "",
+        dispImg: "https://www.martinsound.com/wp-content/uploads/2022/12/DSC04934-1024x576.jpg", 
+        shortDesc: "Set the scene with our Neotek console, Yamaha grand piano, retro mics, and our reel to reel tape recorders",
+        tags: "",
+        longDesc: ""
+    }
+
+    iter+=1; filmSpaces[iter] = {
+        name: "Oval Office", link: "https://www.martinsound.com/oval-office/", bookingLink: "",
+        dispImg: "https://www.martinsound.com/wp-content/uploads/2020/03/DSC02709-scaled.jpg", 
+        shortDesc: "A roughly to-scale standing set of Frump's Oval Office",
+        tags: "",
+        longDesc: ""
+    }
+
+/*
+    iter+=1; filmSpaces[iter] = {
+        name: "", link: "", bookingLink: "",
+        dispImg: "", 
+        shortDesc: "",
+        tags: "",
+        longDesc: ""
+    }
+*/
+
+
